23e2f0ff96257f845bd3f2761d3177406ba3c1bb
tiago.valente <tivalente@gmail.com>
2025-07-17 16:39:54 -0300

    second commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { MenuBarComponent } from './components/menu-bar/menu-bar.component';
+import { BigCardComponent } from './components/big-card/big-card.component';
+import { MenuTitleComponent } from './components/menu-title/menu-title.component';
+import { HomeComponent } from './pages/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [
+    RouterOutlet,
+    MenuBarComponent,
+    BigCardComponent,
+    MenuTitleComponent,
+    HomeComponent,
+  ],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'angular-blog-project';
