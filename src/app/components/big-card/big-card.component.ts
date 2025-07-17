90e7f07f52d0c95e0032a328aa913dde0fe9628f
tiago.valente <tivalente@gmail.com>
2025-07-17 17:50:40 -0300

    components dinamics

--- src/app/components/big-card/big-card.component.ts
+++ src/app/components/big-card/big-card.component.ts
@@ -1,12 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-big-card',
   standalone: true,
   imports: [],
   templateUrl: './big-card.component.html',
-  styleUrl: './big-card.component.css'
+  styleUrl: './big-card.component.css',
 })
 export class BigCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = '';
+
+  @Input()
+  cardDescription: string = '';
 }
--- src/app/components/small-card/small-card.component.ts
+++ src/app/components/small-card/small-card.component.ts
@@ -1,12 +1,16 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-small-card',
   standalone: true,
   imports: [],
   templateUrl: './small-card.component.html',
-  styleUrl: './small-card.component.css'
+  styleUrl: './small-card.component.css',
 })
 export class SmallCardComponent {
+  @Input()
+  photoCover: string = '';
 
+  @Input()
+  cardTitle: string = '';
 }
